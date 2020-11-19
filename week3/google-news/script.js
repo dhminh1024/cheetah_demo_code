a5762737b968b1e95d4a86caf5edea2b57b45cb0
Minh Do <minhdh@coderschool.vn>
2020-11-19 14:55:42 +0700

    load data from google

--- week3/google-news/script.js
+++ week3/google-news/script.js
@@ -1,11 +1,30 @@
 let currentSidemenu = "top-stories";
 const API_KEY = "1b50730b754f46618060e2353ce40222";
+let newsList = [];
 
 const getArticles = async () => {
   const url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
   const response = await fetch(url);
   const data = await response.json();
+  newsList = data.articles;
+  renderArticles(newsList);
   console.log(data);
+};
+
+const renderArticles = (newsList) => {
+  const newsListHTML = newsList
+    .map((news) => {
+      return `<li class="media">
+      <div class="media-body">
+        <h5 class="mt-0 mb-1">${news.title}</h5>
+        <p>${news.content}</p>
+      </div>
+      <img src="${news.urlToImage}" class="ml-3" alt="..." />
+    </li>`;
+    })
+    .join("");
+  console.log(newsListHTML);
+  document.getElementById("news-list").innerHTML = newsListHTML;
 };
 getArticles();
 
