66e34a846ac98c7a4cf8b1da39407f2d0ab0756e
Minh Do <minhdh@coderschool.vn>
2020-11-19 15:39:17 +0700

    adding search

--- week3/google-news/script.js
+++ week3/google-news/script.js
@@ -8,7 +8,6 @@ const getArticles = async () => {
   const data = await response.json();
   newsList = data.articles;
   renderArticles(newsList);
-  console.log(data);
 };
 
 const renderArticles = (newsList) => {
@@ -18,12 +17,14 @@ const renderArticles = (newsList) => {
       <div class="media-body">
         <h5 class="mt-0 mb-1">${news.title}</h5>
         <p>${news.content}</p>
+        <div>
+          <a href="${news.url}" target="_blank">View Full Coverage</a>
+        </div>
       </div>
       <img src="${news.urlToImage}" class="ml-3" alt="..." />
     </li>`;
     })
     .join("");
-  console.log(newsListHTML);
   document.getElementById("news-list").innerHTML = newsListHTML;
 };
 getArticles();
@@ -38,8 +39,37 @@ const putActiveClass = (category) => {
   currentSidemenu = category;
 };
 
-const handleClickSidemenu = (category) => {
+const handleClickSidemenu = async (category) => {
   putActiveClass(category);
+
+  // Load data from the category
+  document.getElementById("news-list").innerHTML = "Loading...";
+  const url = `http://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  renderArticles(newsList);
+};
+
+const handleSearchClick = async () => {
+  try {
+    const q = document.getElementById("search-input").value;
+    document.getElementById("news-list").innerHTML = "Loading...";
+    let url;
+    if (currentSidemenu !== "top-stories") {
+      url = `http://newsapi.org/v2/top-headlines?country=us&category=${currentSidemenu}&q=${q}&apiKey=${API_KEY}`;
+    } else {
+      url = `http://newsapi.org/v2/top-headlines?country=us&q=${q}&apiKey=${API_KEY}`;
+    }
+    console.log(url);
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles;
+    console.log({ q, newsList });
+    renderArticles(newsList);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 putActiveClass(currentSidemenu);
