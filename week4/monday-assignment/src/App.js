572e8d3e5cf80bdc1d630dbd9aba068908869663
Minh Do <minhdh@coderschool.vn>
2020-12-01 06:53:59 +0700

    week 5 starts

--- week4/monday-assignment/src/App.js
+++ week4/monday-assignment/src/App.js
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import AssignmentOne from "./components/AssignmentOne";
 import AssignmentTwo from "./components/AssignmentTwo";
+import HookFlow from "./components/HookFlow";
 
 function App() {
   return (
     <Container>
       {/* <AssignmentOne /> */}
-      <AssignmentTwo />
+      {/* <AssignmentTwo /> */}
+      <HookFlow />
     </Container>
   );
 }
