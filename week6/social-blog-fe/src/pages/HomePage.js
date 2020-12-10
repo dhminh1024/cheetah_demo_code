9fe8911618c3b1c7c2f63f7186c46476d68488f1
Minh Do <minhdh@coderschool.vn>
2020-12-10 12:01:36 +0700

    add pagination

--- week6/social-blog-fe/src/pages/HomePage.js
+++ week6/social-blog-fe/src/pages/HomePage.js
@@ -4,19 +4,27 @@ import { useDispatch, useSelector } from "react-redux";
 import blogActions from "../redux/actions/blog.actions";
 
 import { ClipLoader } from "react-spinners";
+import PaginationBar from "../components/PaginationBar";
 
 const HomePage = () => {
   const blogs = useSelector((state) => state.blog.blogs);
   const totalPageNum = useSelector((state) => state.blog.totalPageNum);
   const loading = useSelector((state) => state.blog.loading);
 
+  const [pageNum, setPageNum] = useState(1);
+
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(blogActions.blogsRequest());
-  }, [dispatch]);
+    dispatch(blogActions.blogsRequest(pageNum));
+  }, [dispatch, pageNum]);
 
   return (
     <Container>
+      <PaginationBar
+        pageNum={pageNum}
+        setPageNum={setPageNum}
+        totalPageNum={totalPageNum}
+      />
       {loading ? (
         <div className="text-center">
           <ClipLoader color="red" size={150} loading={true} />
--- week6/social-blog-fe/src/redux/actions/blog.actions.js
+++ week6/social-blog-fe/src/redux/actions/blog.actions.js
@@ -2,11 +2,11 @@ import * as types from "../constants/blog.constants";
 import api from "../../apiService";
 import { toast } from "react-toastify";
 
-const blogsRequest = () => async (dispatch) => {
+const blogsRequest = (pageNum) => async (dispatch) => {
   dispatch({ type: types.GET_BLOGS_REQUEST, payload: null });
   try {
     // TODO
-    const res = await api.get("api/blogs");
+    const res = await api.get(`api/blogs?page=${pageNum}`);
     dispatch({ type: types.GET_BLOGS_SUCCESS, payload: res.data.data });
   } catch (error) {
     dispatch({ type: types.GET_BLOGS_FAILURE, payload: null });
